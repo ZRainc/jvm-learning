c14042b588142bdca19ce7372c5789bfb53e6da6
zhangyuchen <823731949@qq.com>
2024-03-18 21:00:01 +0800

    Add chapter01 code

---
+++ chapter01/src/main/java/com/rain/jvm/StackStruTest.java
@@ -0,0 +1,19 @@
+package com.rain.jvm;
+
+/**
+ * @ClassName StackStruTest
+ * @Author Rain
+ * @Date 2024/3/18 20:26
+ * @Version 1.0
+ */
+public class StackStruTest {
+
+    // javap -v 编译
+    public static void main(String[] args) {
+        // int i = 2 + 3;
+
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
